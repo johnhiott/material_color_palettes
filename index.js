d10f80731c8360aa404798036c3be1004a7f4832
John Hiott <jhiott@oolcay.com>
2015-09-21 09:44:38 -0400

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+
+var stream = fs.createWriteStream("colors.md");
+
+var url = 'https://www.google.com/design/spec/style/color.html#color-color-palette';
+
+request(url, function(error, response, html){
+
+    // First we'll check to make sure no errors occurred when making the request
+
+    if(!error){
+        // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+        var $ = cheerio.load(html);
+
+        $(".color-group").each(function( index ) {
+          var prefix =  $(this).find(".name").text().replace(' ', '_' ).toLowerCase();
+
+          if (prefix === "") return false;
+
+          var title = '### ' + prefix + '\n\n';
+          stream.write(title);
+
+          stream.write('```\n\n');
+
+          $(this).find(".color").each(function () {
+            var colorTitle = $(this).find(".shade").text().toLowerCase();;
+            var colorHex = $(this).find(".hex").text();
+            if (!$(this).hasClass("main-color")) {
+              stream.write('<color name="' + prefix + "_" + colorTitle + '">' + colorHex + '</color>\n');
+            }
+          });
+          stream.write('```\n\n');
+        });
+
+    }
+});
